0c4960d4f56aa19bd2797200f19278fc8c16fae9
BR1LLsv <brililonchiksv@gmail.com>
2026-08-18 19:57:09 +0300

    Виправлено ALLOWED_HOSTS для Render

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin12345")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
